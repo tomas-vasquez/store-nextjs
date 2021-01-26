85a52c736d50bc5d4c1b3eab71a8f8243d2de2f0
tomasdetloging <tomasdetloging@gmail.com>
2021-01-26 08:15:30 -0400

    add new styles

---
+++ src/pages/_app.js
@@ -0,0 +1,11 @@
+// import "../styles/globals.css";
+
+import "../assets/css/bootstrap.css";
+import "../assets/css/style.css";
+import "../assets/css/stylesheet.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
