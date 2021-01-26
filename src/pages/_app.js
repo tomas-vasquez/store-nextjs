61b23d81bbd982ce1886e27e3262c692347c007c
tomasdetloging <tomasdetloging@gmail.com>
2021-01-26 08:38:32 -0400

    add basic layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,8 +4,19 @@ import "../assets/css/bootstrap.css";
 import "../assets/css/style.css";
 import "../assets/css/stylesheet.css";
 
+import Topbar from "../components/Topbar";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div class="page-container">
+      <Topbar />
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
 
 export default MyApp;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,22 +2,5 @@ import Head from "next/head";
 // import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/vercel.svg" alt="Vercel Logo" />
-        </a>
-      </footer>
-    </div>
-  );
+  return <div>pjdjopajdpi</div>;
 }
